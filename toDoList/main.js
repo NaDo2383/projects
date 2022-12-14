933b7aeae07b881bff0a7faf87f67b1265f3a08e
NaDo2383 <narandorj.byambadorj@gmail.com>
2022-12-14 21:58:42 +0800

    20221214 home

--- toDoList/main.js
+++ toDoList/main.js
@@ -11,17 +11,10 @@ let header = document.getElementById("header");
 let list = document.getElementById("list");
 let todayDate = new Date();
 
-let list_ToDo = document.createElement("div");
-let list_ToDo_Name = document.createElement("p");
-let list_ToDo_icons = document.createElement("div");
-let list_ToDo_icon1 = document.createElement("i");
-let list_ToDo_icon2 = document.createElement("i");
-let list_ToDo_icon3 = document.createElement("i");
 let divIDname;
 
 header_h1.innerHTML = "ToDo list app";
 header_date.innerHTML = `Today date: ${todayDate.getFullYear()}-${todayDate.getMonth() + 1}-${todayDate.getDate()}`;
-header_left_tasks.innerHTML = "1 task left";
 header_task_button.innerHTML = "Add ToDo";
 list_h1.innerHTML = "ToDo lists";
 header_date_and_ltasks.id = "header_date_and_ltasks";
@@ -39,22 +32,46 @@ header.appendChild(header_task);
 header_task.appendChild(header_task_input);
 header_task.appendChild(header_task_button);
 list.appendChild(list_h1);
-list_ToDo.appendChild(list_ToDo_Name);
-list_ToDo.appendChild(list_ToDo_icons);
-list_ToDo_icons.appendChild(list_ToDo_icon1);
-list_ToDo_icons.appendChild(list_ToDo_icon2);
-list_ToDo_icons.appendChild(list_ToDo_icon3);
 
 function addToDolist() {
     list_lists.push(header_task_input.value);
-    for (let i = 0; i < list_lists.length; i++) {
-        list_ToDo_Name.innerHTML = list_lists[i];
-        let ToDo = document.createElement("div");
-        ToDo.innerHTML = "<div class='ToDo' id='${ i}'></div>";
-
+    let ToDo;
+    for (let j = 0; j < list_lists.length - 1; j++) {
+        list.removeChild(list.lastChild);
     }
-
+    for (let i = 0; i < list_lists.length; i++) {
+        ToDo = document.createElement("div");
+        ToDo.className = "ToDo"
+        ToDo.innerHTML = `<div class="ToDo_name">${list_lists[i]}</div><i class="fa-solid fa-pen edit${i} icons"></i><i class="fa-regular fa-square-check checkmark${i} icons" onclick="checkMark()"></i><i class="fa-solid fa-trash-can remove${i} icons remove"></i>`;
+        list.appendChild(ToDo);
+    }
+    ToDo.class = "ToDo";
     header_task_input.value = "";
+    if (list_lists.length > 0) {
+        header_left_tasks.innerHTML = `${list_lists.length} task left`;
+    } else {
+        header_left_tasks.innerHTML = "";
+    }
 }
 
 header_task_button.addEventListener("click", addToDolist);
+
+let editBtn = document.querySelector("#edit");
+let checkMarkBtn = document.querySelector("#checkmark");
+let removeBtn = document.querySelector("#remove");
+
+function edit() {
+
+}
+function checkMark() {
+    event.srcElement.previousElementSibling.previousElementSibling.style.textDecorationLine = "line-through"
+}
+// function remove() {
+//     let delete;
+//     for (let l = 0; l < list_lists.length; l++) {
+//         delete =
+//         if ()
+        
+//     }
+// }
+
